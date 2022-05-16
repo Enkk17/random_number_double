d9dc8dfd17458263c5047ef6306638d664bbc66b
Enkk17 <86929986+Enkk17@users.noreply.github.com>
2022-05-16 21:03:13 +0200

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+double* generatore(int n);
+void stampa(double* n,int totale);
+
+int main()
+{
+	double* numeri;
+	int totale;
+	cout << "quanti numeri vuoi generare? ";
+	cin >> totale;
+	numeri = new double[totale];
+	numeri=generatore(totale);
+	stampa(numeri,totale);
+	return 0;
+
+}
+
+double* generatore(int n)
+{
+	srand(time(NULL));
+	double* array=new double[n];
+	double r;
+	for (int j = 0; j < n;j++)
+	{
+		//generiamo un numero di tipo double tenendo in considerazione i numeri fra 100.0 a 0
+		r=((double)rand() * (100.0 - 0) / (double)RAND_MAX - 0);
+		array[j] = r;
+	}
+	return array;
+}
+
+void stampa(double n[], int totale)
+{
+	for (int j = 0; j < totale;j++)
+	{
+		cout << n[j] << endl;
+	}
+}
