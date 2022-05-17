ea912e6ab41ca82b1adfb1d86f4dda4893b9cd5e
Enkk17 <86929986+Enkk17@users.noreply.github.com>
2022-05-17 11:46:08 +0200

    Update main.cpp
    Clear code

--- main.cpp
+++ main.cpp
@@ -8,35 +8,34 @@ void stampa(double* n,int totale);
 
 int main()
 {
-	double* numeri;
-	int totale;
-	cout << "quanti numeri vuoi generare? ";
-	cin >> totale;
-	numeri = new double[totale];
-	numeri=generatore(totale);
-	stampa(numeri,totale);
-	return 0;
-
+double* numeri;
+int totale;
+cout << "quanti numeri vuoi generare? ";
+cin >> totale;
+numeri = new double[totale];
+numeri=generatore(totale);
+stampa(numeri,totale);
+return 0;
 }
 
 double* generatore(int n)
 {
-	srand(time(NULL));
-	double* array=new double[n];
-	double r;
-	for (int j = 0; j < n;j++)
-	{
-		//generiamo un numero di tipo double tenendo in considerazione i numeri fra 100.0 a 0
-		r=((double)rand() * (100.0 - 0) / (double)RAND_MAX - 0);
-		array[j] = r;
-	}
-	return array;
+srand(time(NULL));
+double* array=new double[n];
+double r;
+for (int j = 0; j < n;j++)
+{
+//generiamo un numero di tipo double tenendo in considerazione i numeri fra 100.0 a 0
+r=rand() / (double)RAND_MAX * 100;
+array[j] = r;
+}
+return array;
 }
 
 void stampa(double n[], int totale)
 {
-	for (int j = 0; j < totale;j++)
-	{
-		cout << n[j] << endl;
-	}
+for (int j = 0; j < totale;j++)
+{
+cout << n[j] << endl;
+}
 }
